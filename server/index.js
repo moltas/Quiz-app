931415cc89adec13a1d9f0d2c40ae7280af1a423
Tobias Johansson <T_johansson92@hotmail.com>
2015-05-05 18:13:31 +0200

    serving bower files in index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(bodyParser());
 
 //files
 app.use(express.static(__dirname + '/../client'));
-app.use(express.static(__dirname + '/../client/.tmp'));
 app.use(express.static(__dirname + '/../client/app'));
-app.use(express.static(__dirname + '/../client/bower_components'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 
 
 //API
